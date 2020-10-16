0db7defc69238757dd679791dcda722e3afafa59
halit <halitfuatbatur@gmail.com>
2020-10-16 11:27:44 +0300

    fixed the snapshot and removed the fragment in MemoryList

--- src/containers/MemoriesListContainer.js
+++ src/containers/MemoriesListContainer.js
@@ -1,61 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import db from '../firebaseConfig';
+import React, { useState, useEffect } from "react";
+import db from "../firebaseConfig";
 import MemoriesList from "../components/memory/MemoriesList";
 import { useParams } from "react-router-dom";
 
-
 export default function MemoriesListContainer() {
-  const [memories, setMemories] = useState([])
+  const [memories, setMemories] = useState([]);
 
-  const [albumDetail, setAlbumDetail] = useState('');
+  const [albumDetail, setAlbumDetail] = useState("");
 
   const fetchAlbumDetail = async () => {
-    const res = await db.collection('Albums').doc(albumId).get()
+    const res = await db.collection("Albums").doc(albumId).get();
     setAlbumDetail(res.data());
-  }
-  useEffect(() => { fetchAlbumDetail() }, []);
+  };
+  useEffect(() => {
+    fetchAlbumDetail();
+  }, []);
 
   let { albumId } = useParams();
 
   // console.log("album", albumDetail)
 
   useEffect(() => {
-    db.collection("Albums").doc(albumId).collection("Memories").onSnapshot(function (snapshot) {
-      let memoriesList = [...memories];
-      snapshot.docChanges().forEach(function (change) {
-        console.log(change);
-        if (change.type === "added") {
-          const memory = {
-            id: change.doc.id,
-            data: change.doc.data()
+    db.collection("Albums")
+      .doc(albumId)
+      .collection("Memories")
+      .onSnapshot(function (snapshot) {
+        snapshot.docChanges().forEach(function (change) {
+          // console.log(change);
+
+          if (change.type === "added") {
+            const memory = {
+              id: change.doc.id,
+              data: change.doc.data(),
+            };
+            setMemories((prevMemories) => [...prevMemories, memory]);
+            // console.log("after add", memoriesList);
           }
-          memoriesList.push(memory)
-          console.log("after add", memoriesList);
-        }
-        if (change.type === "modified") {
-          const moditiedMemoryIndex = memoriesList.findIndex(memory => memory.id === change.doc.id);
-
-          const modifiedMemory = {
-            id: change.doc.id,
-            data: change.doc.data()
+          if (change.type === "modified") {
+            const modifiedMemory = {
+              id: change.doc.id,
+              data: change.doc.data(),
+            };
+            setMemories((prevMemories) => {
+              let newMemories = [...prevMemories];
+              const moditiedMemoryIndex = newMemories.findIndex(
+                (memory) => memory.id === change.doc.id
+              );
+              newMemories.splice(moditiedMemoryIndex, 1, modifiedMemory);
+              return newMemories;
+            });
           }
-          memoriesList.splice(moditiedMemoryIndex, 1, modifiedMemory);
-
-          console.log("Modified: ", memoriesList);
-        }
-        if (change.type === "removed") {
-          const removedId = change.doc.id;
-          memoriesList = memoriesList.filter(memory => memory.id !== removedId);
-          console.log("deletion", memoriesList);
-        }
-
-        setMemories([...memoriesList])
+          if (change.type === "removed") {
+            const removedId = change.doc.id;
+            setMemories((prevState) => {
+              let newMemories = [...prevState];
+              newMemories = newMemories.filter(
+                (memory) => memory.id !== removedId
+              );
+              return newMemories;
+            });
+            console.log("somethingi is removed !");
+          }
+        });
       });
-    });
-
-  }, [])
+  }, []);
 
   return (
-    <MemoriesList memories={memories} albumName={albumDetail ? albumDetail.name : null} albumId={albumId} setMemories={setMemories} />
-  )
+    <MemoriesList
+      memories={memories}
+      albumName={albumDetail ? albumDetail.name : null}
+      albumId={albumId}
+      setMemories={setMemories}
+    />
+  );
 }
