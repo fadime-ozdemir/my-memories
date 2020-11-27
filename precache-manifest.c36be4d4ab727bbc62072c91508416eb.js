e7e8de70bebe6801056cacbf20312d8ac6b1fbaa
fadime94 <fadime.ozdemir11@gmail.com>
2020-11-27 14:43:39 +0300

    Updates

---
+++ precache-manifest.c36be4d4ab727bbc62072c91508416eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a1751f794366879022334122d7a022",
+    "url": "/my-memories/index.html"
+  },
+  {
+    "revision": "0e39064fe3531323e2ce",
+    "url": "/my-memories/static/css/2.a20d75c9.chunk.css"
+  },
+  {
+    "revision": "2c9747a5d7d60c1109de",
+    "url": "/my-memories/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "0e39064fe3531323e2ce",
+    "url": "/my-memories/static/js/2.45d22925.chunk.js"
+  },
+  {
+    "revision": "fcbf42754ab0b66528572837cd3c01e5",
+    "url": "/my-memories/static/js/2.45d22925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c9747a5d7d60c1109de",
+    "url": "/my-memories/static/js/main.2c450248.chunk.js"
+  },
+  {
+    "revision": "0d989c86872cf85ce0ff",
+    "url": "/my-memories/static/js/runtime-main.e864d4c4.js"
+  },
+  {
+    "revision": "10c545872e8ab0089292496da40fe350",
+    "url": "/my-memories/static/media/keep-memories.10c54587.png"
+  }
+]);
